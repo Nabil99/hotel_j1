d363104e2c904c726adca5606396cff7f1c56a46
Shaikh Nabil <shaikhnabil9@gmail.com>
2019-07-28 17:25:16 +0530

    go

---
+++ src/app/api-service.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+  private apiUrl = "http://192.168.0.20:7825/interceptor.go";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Accept: 'text/plain'
+    })
+  };
+  constructor(private http: HttpClient) { }
+
+  addHotel(value: any): Promise<any> {
+    console.log(value);
+    const dataValue = {
+      function: 'AddHotel',
+      args: value
+    }
+    
+    return this.http.post(this.apiUrl, dataValue,  this.httpOptions)
+        .toPromise()
+        .then(this.extractData)
+        .catch(this.handleErrorObservable);
+  }
+  private extractData(res: Response) {
+    const body = res.json();
+    return body || {};
+  }
+  
+  private handleErrorObservable (error: Response | any) {
+    console.error(error);
+    return Observable.throw(error);
+  }
+}
+
+
+
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, FormArray, FormBuilder} from '@angular/forms';
+import { element } from 'protractor';
+import { ApiServiceService } from './api-service.service'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  fruits : Array<String> = ['Admin', 'Waiter', 'Kitchen'];
+  selectedFruitValues = [];
+  constructor(private fb: FormBuilder, private _http:ApiServiceService){
+
+  }
+
+ngOnInit(){
+
+}
+
+  //title = 'HotelEntry';
+  createHotelForm = this.fb.group({
+    hotelName : [''],
+    contactNo : [''],
+    ownerName : [''],
+    ownerEmail : [''],
+    password : [''],
+    hotelBankDetails : [''],
+    bankName : [''],
+    accountNo : [''],
+    ifscNo : [''],
+    accType : [''],
+    ownerAadhar : [''],
+    panCardNo : [''],
+    saledid : [''],
+    plan : [''],
+    views : [''],
+    favFruits : this.addFruitsControls()
+  });
+
+  addFruitsControls(){
+    const arr = this.fruits.map(element => {
+      return this.fb.control(false);
+    });
+
+    return this.fb.array(arr);
+  }
+
+  get fruitsArray() {
+    return <FormArray>this.createHotelForm.get('favFruits');
+  }
+
+  getselectedFruitValues() {
+    this.selectedFruitValues = [];
+    this.fruitsArray.controls.forEach((control, i)=>{
+      if (control.value){
+        this.selectedFruitValues.push(this.fruits[i]);
+      }
+    });
+
+    console.log(this.selectedFruitValues);
+  }
+
+  
+  
+  onSubmit() {
+    console.log(this.createHotelForm.value);
+    this._http.addHotel(this.createHotelForm.value);
+  }
+
+}
